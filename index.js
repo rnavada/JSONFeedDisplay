1b9aa9ccbe1d0b855ff7baa51f50c7d046ab6a94
rnavada <raghav.navada@yahoo.com>
2019-05-11 06:46:49 -0700

    playing around with canvas

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-//var c = document.getElementById("myCanvas");
-//var ctx = c.getContext("2d");
-//var img = document.getElementById("scream");
-//ctx.drawImage(img,100,109);
+/*
+ * Q!.
+ * 
+ */
 
 function main() {
   console.log('hello world');
@@ -14,9 +14,18 @@ function main() {
     console.log(myJson);
     console.log(myJson.dates[0].games[0]);
     //console.log(JSON.stringify(myJson));
+
+    //var c = document.getElementById("myCanvas");
+    //var ctx = c.getContext("2d");
+    //ctx.font = "15px Arial";
+    //ctx.fillText(myJson.dates[0].games[0].gamePk + '', 10, 50);
+
+    
+
   });
 
 
 }
 
 main();
+
